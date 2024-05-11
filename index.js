3f9cdc9abdaa4c89d2f4f3230e73a071793b5359
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2024-05-11 22:09:42 +0530

    timing log

--- index.js
+++ index.js
@@ -15,8 +15,23 @@ app.get("/", (req, res) => {
   res.send("Welcome to our Hotel");
 });
 
+//Middleware Function
+
+const logRequest = (req, res, next) => {
+  console.log(
+    `${req.method} method is requested at ${
+      req.url
+    } on ${new Date().toLocaleString()}`
+  );
+  next();
+};
+
+//Routes
+
 const personRoute = require("./routes/PersonRouute");
 const menuRoute = require("./routes/MenuRoute");
+
+app.use(logRequest);
 
 app.use("/person", personRoute);
 app.use("/menu", menuRoute);
